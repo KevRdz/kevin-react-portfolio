4031b05adb79a4778251e85b1372dce4e2cbfbf8
Kevin <kevin.rodriguez678@gmail.com>
2022-10-25 15:42:22 -0400

    add component folder and create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] justify-between items-center px-4 bg-slate-900 text-gray-400'>
+
+    </div>
+  )
+}
+
+export default Navbar
