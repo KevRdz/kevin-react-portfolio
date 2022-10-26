b24196bf01800212888f085964b6e582d9e31633
Kevin <kevin.rodriguez678@gmail.com>
2022-10-26 14:47:07 -0400

    add li to navbar and import logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Logo from "../assets/KR-Logo.png"
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] justify-between items-center px-4 bg-slate-900 text-gray-400'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-400'>
+      <div>
+        <img src={Logo} alt="Logo Img" style={{ width: "80px"}} />
+      </div>
 
+      <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className='hidden'>
+        <FaBars />
+      </div>
+
+      <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+
+      <div className='hidden'></div>
     </div>
   )
 }
