25ece733a2ea41a855d998faba0aeb2b9f46d5f1
Kevin <kevin.rodriguez678@gmail.com>
2022-10-26 15:42:03 -0400

    add ssocial icons to to page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Logo from "../assets/KR-Logo.png"
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -32,7 +34,30 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>Contact</li>
       </ul>
 
-      <div className='hidden'></div>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#0072b1]'>
+            <a className='flex justify-between items-center w-full text-white' href='/'>
+              Linkedin <FaLinkedin size={50}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-[#171515]'>
+            <a className='flex justify-between items-center w-full text-white' href='/'>
+              GitHub <FaGithub size={50}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-orange-500'>
+            <a className='flex justify-between items-center w-full text-white' href='/'>
+              E-mail <HiOutlineMail size={50}/>
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-10px] duration-300 bg-slate-400'>
+            <a className='flex justify-between items-center w-full text-white' href='/'>
+              Resume <BsFillPersonLinesFill size={50}/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
