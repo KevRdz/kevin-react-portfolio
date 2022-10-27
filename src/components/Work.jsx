f5c1d487ab458302ee65bd0415a71c921aa6b3cb
Kevin <kevin.rodriguez678@gmail.com>
2022-10-27 15:51:15 -0400

    fix background image on grid items

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import newtImg from '../assets/newt.png'
+import fish from '../assets/fish-tracker.png'
 
 const Work = () => {
   return (
@@ -8,8 +10,40 @@ const Work = () => {
           <p className='text-4xl font-bold inline border-b-4 text-gray-400 border-red-500'>Work</p>
           <p className='py-6'>Check Out some of my recent work</p>
         </div>
+        {/* Container*/}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='shawdow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+          {/* Grid Item */}
+          <div style={{backgroundImage: `url(${newtImg})` }} className='shawdow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="home">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-400 font-bold text-lg'>Deployed</button>
+                </a>
+                <a href="home">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-400 font-bold text-lg'>GitHub</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${fish})` }} className='shawdow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS App
+              </span>
+              <div className='pt-8 text-center'>
+                <a href="home">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-400 font-bold text-lg'>Deployed</button>
+                </a>
+                <a href="home">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-400 font-bold text-lg'>GitHub</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${newtImg})` }} className='shawdow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS App
