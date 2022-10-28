298e01495b9a94c07cf93f041920934fa2657cbd
Kevin <kevin.rodriguez678@gmail.com>
2022-10-28 14:20:33 -0400

    add react scroll package

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='conatct' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       <form method='POST' action="https://getform.io/f/2c3baded-147d-43af-8a4b-979d2852a5bb" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-red-600 text-gray-500'>Contact</p>
