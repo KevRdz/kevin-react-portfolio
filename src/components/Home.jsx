142b2444bb4ea3f5a2d3578c7b8d197e790da6fa
Kevin <kevin.rodriguez678@gmail.com>
2022-10-31 16:34:40 -0400

    fix link and button for react scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -11,13 +11,13 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Full-Stack Developer</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer specializing in creating unique and fun digital experiences. I have a passion for creating exceptional web apps that are both mobile responsive and user-friendly. Currently, I'm enjoying working front-end with React.js and Tailwind.</p>
         <div>
-          <button to="work" className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
-            <Link to="work" smooth={true} duration={500}>
+          <button>
+            <Link to="work" className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600' smooth={true} duration={500}>
               View Work
+              <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3'/>
+              </span>
             </Link>
-            <span className='group-hover:rotate-90 duration-300'>
-              <HiArrowNarrowRight className='ml-3'/>
-            </span>
           </button>
         </div>
       </div>
